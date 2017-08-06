75460ee426cf459fe627a8d3157fc8856e0b946c
Natalia <n.powalska@gmail.com>
2017-08-06 13:10:29 +0200

    Add project files

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'main.css': 'main.sass'
+  			}
+  		}
+  	},
+
+    jshint: {
+      all: ['js/*.js']
+    }
+
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Default task(s).
+
+  grunt.registerTask('default', ['sass', 'jshint']);
+};
